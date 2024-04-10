8f41b527cf4f90e2355e137213425cf0252f6f84
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2024-04-10 20:17:36 -0300

    Exercicio BD I, Desenvolvimento 1

---
+++ Aula_aoVivo/aoVivo_20/index.js
@@ -0,0 +1,132 @@
+// console.log("Hello, World!!");
+
+/* 
+let username = "Walace";
+
+console.log(username); 
+
+comando para comentar um bloco de código:
+ - em casa: " Ctrl + ;"
+ - no senai: " Ctrl + / "
+
+*/
+
+
+// let username = "Walace";
+// console.log(username); 
+// username = "Rafael";
+// console.log(username); 
+
+
+// const nomes = ["Ruth", "Ana", "Simone", "Lenilson"];
+// console.log(nomes); 
+// nomes[0] = "Gustavo";
+// console.log(nomes); 
+
+
+
+// const id = 123;
+// const username = "Mateus";
+// if ( id === 123 || username === "Mateus"){
+//   console.log("Boas Vindas!")
+// } else {
+//   console.log("Não autorizado!")
+// }
+
+
+
+// let id = 123;
+// id = 456
+// let username = "Mateus";
+// username = "Sara"
+// if ( id === 123 || username === "Mateus"){
+//   console.log("Boas Vindas!")
+// } else {
+//   console.log("Não autorizado!")
+// }
+
+
+// const informacoes_usuario = ["Ruth", 123 , "123456", true ];
+
+
+
+// const usuario = {
+//     nome: "Michele",
+//     id: 678,
+//     senha: "afdfjsdjfdkfjskl",
+//     admin: true
+//   };
+  
+//   console.log(   usuario           );
+//   console.log(   usuario["nome"]   );
+//   console.log(   usuario["id"]     );
+//   console.log(   usuario.senha     );
+//   console.log(   usuario.admin     );
+
+
+// const usuario1 = {
+//     nome: "Michele",
+//     id: 678,
+//     senha: "afdfjsdjfdkfjskl",
+//     admin: true
+//   };
+//   usuario2: {
+//   };
+//   console.log(console)
+
+
+// const usuario = {
+//     nome: "Michele",
+//     id: 678,
+//     senha: "afdfjsdjfdkfjskl",
+//     admin: true
+//   };
+//   console.log(   usuario           );
+//   console.log(   usuario["nome"]   );
+//   console.log(   usuario["id"]     );
+//   console.log(   usuario.senha     );
+//   console.log(   usuario.admin     );
+//   if (usuario.nome === "Michele" && usuario.senha === "afdfjsdjfdkfjskl"){
+//     console.log("Boas Vindas!")
+//   } else{
+//     console.log("credenciais invalidas!")
+//   }
+
+
+// const usuario = {
+//     nome: "Michele",
+//     id: 678,
+//     senha: "afdfjsdjfdkfjskl",
+//     admin: true
+//   };
+//   if (usuario.nome === "Michele" && usuario.senha === "afdfjsdjfdkfjskl"){
+//     console.log("Boas Vindas!")
+//   } else{
+//     console.log("credenciais invalidas!")
+//   }
+
+
+// const usuario = {
+//     nome: "Michele",
+//     id: 678,
+//     senha: "afdfjsdjfdkfjskl9",
+//     admin: true
+//   };
+//   if (usuario.nome === "Michele" && usuario.senha === "afdfjsdjfdkfjskl"){
+//     console.log("Boas Vindas!")
+//   } else{
+//     console.log("credenciais invalidas!")
+//   }
+
+
+// const usuario = {
+//     nome: "Michele",
+//     id: 678,
+//     senha: "afdfjsdjfdkfjskl",
+//     admin: true
+//   };
+//   if (usuario.nome === "Michele" && usuario.senha === "afdfjsdjfdkfjskl"){
+//     console.log("Boas Vindas,"+ usuario.nome + "!")
+//   } else{
+//     console.log("credenciais invalidas!")
+//   }

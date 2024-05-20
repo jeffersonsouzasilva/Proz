40c1c7e15e236de938717091085ca5c68fc0ec3e
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2024-05-20 20:36:54 -0300

    Pratica guiada 1, aula ao vivo 23

--- Aula_aoVivo/JavaScript_II/aoVivo_23/aula-style-classList/script.js
+++ Aula_aoVivo/JavaScript_II/aoVivo_23/aula-style-classList/script.js
@@ -1,22 +1,36 @@
 // alert()
-const h2 = document.querySelector("h2")
+const h2 = document.querySelector("h2");
 // h2.style.color = "red"
 // h2.style.fontSize = "100px" // propriedade do CSS: font-size
+h2.style.color = "blue";
+h2.style.fontSize = "50px"; // propriedade do CSS: font-size
 
-h2.style.color = "blue"
-h2.style.fontSize = "50px" // propriedade do CSS: font-size
+const username = document.querySelector("#login-usuario");
+const errorText = document.querySelectorAll(".error-text");
+const password = document.querySelector("#login-senha");
 
 // o usuário digitou um nome inválido
-const username = document.querySelector("#login-usuario")
-// username.style.backgroundColor = "red"
-username.classList.add("error")
-// username.style.border = "solid red"
-
-const errorText = document.querySelector(".error-text")
-errorText.classList.add("visible")
-
-// const form = document.querySelector("form")
-// form.style.display = "none"
-
+function mostrarErroUsername() {
+  // username.style.backgroundColor = "red"
+  username.classList.add("error");
+  // username.style.border = "solid red"
+  // const form = document.querySelector("form")
+  // form.style.display = "none"
+  errorText[0].classList.add("visible");
+}
 
 //o usuário corrigiu o nome, mas digitou uma senha inválida /min 1:01:23
+function mostrarSucessoUsername() {
+  username.classList.remove("error");
+  errorText[0].classList.remove("visible");
+  username.classList.add("correct");
+}
+
+function mostrarErroSenha() {
+  password.classList.add("error");
+  errorText[1].classList.add("visible");
+}
+
+mostrarErroUsername();
+mostrarSucessoUsername();
+mostrarErroSenha();

0acfc7e4463b8915a2c87391971424e86d6466a4
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2024-05-25 21:37:42 -0300

    Pratica guiada 1, aula ao vivo 26

--- Aula_aoVivo/JavaScript_II/aoVivo_26/validacao-formulários-01/script.js
+++ Aula_aoVivo/JavaScript_II/aoVivo_26/validacao-formulários-01/script.js
@@ -4,9 +4,24 @@ let usernameLabel = document.querySelector('label[for="username"]');
 let usernameHelper = document.getElementById("username-helper");
 
 // Mostrar popup de campo obrigatório
+function mostrarPopup() {
+  usernameLabel.classList.add("required-popup");
+}
 
+usernameInput.addEventListener("focus", mostrarPopup);
 
 // Ocultar popup de campo obrigatório
+function ocultarPopup(){
+    usernameLabel.classList.remover("required-popup")
+}
 
+
+usernameInput.addEventListener("blur", ocultarPopup)
 
 // Validar valor do input
+
+//testes
+// usernameInput.classList.add("correct")
+// usernameLabel.classList.add("required-popup")
+// usernameHelper.classList.add("visible")
+// usernameHelper.innerText = "Nova mensagem"

8d3268a86319157f7715ff107fc2859848333932
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2024-05-27 23:20:35 -0300

    Prática guiada 2 e 3, aula ao vivo 26

--- Aula_aoVivo/JavaScript_II/aoVivo_26/validacao-formulários-01/script.js
+++ Aula_aoVivo/JavaScript_II/aoVivo_26/validacao-formulários-01/script.js
@@ -11,17 +11,102 @@ function mostrarPopup() {
 usernameInput.addEventListener("focus", mostrarPopup);
 
 // Ocultar popup de campo obrigatório
-function ocultarPopup(){
-    usernameLabel.classList.remover("required-popup")
+function ocultarPopup() {
+  usernameLabel.classList.remover("required-popup");
 }
 
-
-usernameInput.addEventListener("blur", ocultarPopup)
+usernameInput.addEventListener("blur", ocultarPopup);
 
 // Validar valor do input
 
-//testes
+function validarUsername(evento) {
+  const valor = evento.target.value;
+  // console.log(evento.target.value)
+  console.log(valor);
+  // console.log(usernameInput.value)
+
+  if (valor.length < 3) {
+    console.log("valor inválido");
+    usernameInput.classList.remove("correct");
+    usernameInput.classList.add("error");
+    usernameHelper.innerHTML = "Campo obrigatório";
+    // if (valor.length === 0) {
+    //   usernameHelper.innerHTML = "Campo obrigatório";
+    // } else {
+    //   usernameHelper.innerHTML = "Mínimo 3 caracteres";
+    // }
+    usernameHelper.classList.add("visible");
+  } else {
+    console.log("valor válido");
+    usernameInput.classList.add("correct");
+    usernameInput.classList.remove("error");
+    usernameHelper.classList.remove("visible");
+  }
+}
+
+// function validarUsername(evento) {
+//   const valor = evento.target.value;
+//   console.log(valor);
+//   // console.log(usernameInput.value)
+
+//   if (valor.length < 3) {
+//     alert("valor inválido")
+//   } else {
+//     alert("valor válido")
+//   }
+// }
+
+usernameInput.addEventListener("change", validarUsername);
+
+//TESTES
+
 // usernameInput.classList.add("correct")
 // usernameLabel.classList.add("required-popup")
 // usernameHelper.classList.add("visible")
 // usernameHelper.innerText = "Nova mensagem"
+
+// Validação do campos de email
+
+const labelEmail = document.querySelector("label[for='email']");
+const inputEmail = document.getElementById("email");
+const helperEmail = document.getElementById("email-helper");
+
+// console.log(labelEmail, inputEmail, helperEmail)
+
+inputEmail.addEventListener("focus", function () {
+  labelEmail.classList.add("required-popup");
+});
+
+inputEmail.addEventListener("blur", function () {
+  labelEmail.classList.remove("required-popup");
+});
+
+inputEmail.addEventListener("change", function (evento) {
+  const valor = evento.target.value;
+
+  // const emailValido = valor.includes("@") &&
+  // (valor.includes(".com") || valor.includes(".gov") || valor.includes(".dev") );
+
+  // // if(valor.includes("@") && valor.includes(".com")){
+  // //   // alert("email válido")
+
+  // // } else {
+  // //   // alert("email inválido")
+  // // }
+
+  // if (emailValido) {
+  //   // alert("email válido")
+  // } else {
+  //   // alert("email inválido")
+  // }
+
+  if (valor.includes("@")) {
+    // alert("email válido")
+    inputEmail.classList.add("correct");
+    inputEmail.classList.remove("error");
+  } else {
+    // alert("email inválido")
+    inputEmail.classList.add("error");
+    inputEmail.classList.remove("correct");
+  }
+});
